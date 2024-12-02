a62035de3536e990e83df949b5f86d19a4043186
Pedro Henrique <pedrohal2503@gmail.com>
2024-12-01 21:47:41 -0300

    criando teste para trazer uma carta aleatória

---
+++ test/card-test.ts
@@ -0,0 +1,27 @@
+import { Test } from '@nestjs/testing';
+import { CardsController } from '../cards.controller';
+import { CardsService } from '../cards.service';
+
+describe('CardsController', () => {
+    let cardsCtrlr: CardsController;
+    let cardsService: CardsService;
+
+    beforeEach(async () => {
+        const moduleRef = await Test.createTestingModule({
+            controllers: [CardsController],
+            providers: [CardsService],
+        }).compile();
+
+        cardsService = await moduleRef.resolve(CardsService);
+        cardsCtrlr = await moduleRef.resolve(CardsController);
+    });
+
+    describe('findRandomCard', () => {
+        it('should return a random card', async () => {
+            const randomCard = await cardsCtrlr.findRandomCard();
+            jest.spyOn(cardsService, 'findRandom').mockImplementation(() => randomCard);
+
+            expect(await cardsCtrlr.findRandomCard()).toBe(randomCard);
+        })
+    });
+})
