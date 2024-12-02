8fbc03e208a2962a2fc9cd9f0771adc53569b600
Pedro Henrique <pedrohal2503@gmail.com>
2024-12-01 22:05:55 -0300

    Teste para retornar um commander, por Id

--- test/commander-test.ts
+++ test/commander-test.ts
@@ -1,4 +1,27 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { Test } from '@nestjs/testing';
+import { CommandersController } from '../src/commanders/commanders.controller';
+import { CommandersService } from '../src/commanders/commanders.service';
+
+describe('CommandersController', () => {
+    let commandersCtrlr: CommandersController;
+    let commandersService: CommandersService;
+
+    beforeEach(async () => {
+        const moduleRef = await Test.createTestingModule({
+            controllers: [CommandersController],
+            providers: [CommandersService],
+        }).compile();
+
+        commandersService = await moduleRef.resolve(CommandersService);
+        commandersCtrlr = await moduleRef.resolve(CommandersController);
+    });
+
+    describe('getCommanderById', () => {
+        it('should return an especific commander, by its ID', async () => {
+            const commander = await commandersCtrlr.getCommanderById("1");
+            jest.spyOn(commandersService, 'findRandom').mockImplementation(() => commander);
+
+            expect(await commandersCtrlr.getCommanderById("1")).toBe(commander);
+        })
+    });
+})
